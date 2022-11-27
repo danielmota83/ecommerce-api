956ca33f8e62580cc20cec5eab21977b2dd65adb
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-11-27 09:36:45 -0300

    chore: main.ts configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+
+  const config = new DocumentBuilder()
+    .setTitle('e-commerce')
+    .setDescription('Aplicação teste')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
