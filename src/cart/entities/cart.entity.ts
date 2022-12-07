7239847cf660a7ea83146a0a3199b6ec8da6074d
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-12-07 15:05:55 -0300

    chore: updating cart entity

--- src/cart/entities/cart.entity.ts
+++ src/cart/entities/cart.entity.ts
@@ -1,12 +1,10 @@
 import { Order } from 'src/order/entities/order.entity';
 import { User } from 'src/user/entities/user.entity';
 import { UserAddress } from 'src/UserAddress/entities/userAddress.entity';
-import { Payment } from 'src/payment/entities/payment.entity';
 export class Cart {
   id?: string;
   user?: User;
   order?: Order[];
-  payment?: Payment;
   userAddress?: UserAddress[];
   shipping: number;
   totalPrice: number;
