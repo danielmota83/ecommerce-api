4cab98fd0d13d95d173b2625736ac9009dd28626
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-12-06 21:47:28 -0300

    chore: update order entity

--- src/order/entities/order.entity.ts
+++ src/order/entities/order.entity.ts
@@ -1,8 +1,10 @@
 import { Product } from 'src/products/entities/product.entity';
+import { User } from 'src/user/entities/user.entity';
 
 export class Order {
   id: string;
   orderDetails: string;
+  user?: User;
   products?: Product[];
   createdAt?: Date;
   updatedAt?: Date;
