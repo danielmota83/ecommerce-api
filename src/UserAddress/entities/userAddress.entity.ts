ce73f2a1c4bcd3ea7a7b2f64f3cf268750616f21
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-12-11 12:45:42 -0300

    chore: add enum

--- src/UserAddress/entities/userAddress.entity.ts
+++ src/UserAddress/entities/userAddress.entity.ts
@@ -1,9 +1,16 @@
 import { User } from 'src/user/entities/user.entity';
 
+export enum AddressType {
+  residential,
+  commercial,
+  relatives,
+  neighbors,
+  other,
+}
 export class UserAddress {
   id?: string;
   addressType: string;
-  addressInfo: string;
+  addressInfo: AddressType;
   user?: User;
   createdAt?: Date;
   updatedAt?: Date;
